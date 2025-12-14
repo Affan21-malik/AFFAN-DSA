8d45ced7328f680f7bcff180a0dd895a11723d0a
Mohd Affan <mohdaffanboy786@gmail.com>
2025-12-14 18:42:27 +0530

    tut

---
+++ tut4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+/*int global = 6;
+void sum(){
+    int a ;
+    cout<<global;
+}*/
+int main(){
+  // int global = 9;
+    //global = 78;
+    int a=5;
+    int b=8;
+    float pi= 3.222;
+    char c = 'd';
+   /* bool n = true;
+ sum();
+ cout<<global<<n;*/
+cout<<"this  is tutorial 4:\n here the value of a is "<<a<<" \nthe value of b is "<<b;
+cout<<" \nthe value of pi is " <<pi<<"\n the value of c is "<<c;
+    return 0;
+}
