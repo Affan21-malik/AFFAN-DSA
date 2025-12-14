7440d1bf778e657e8f0ada5fdb25b208191589cf
Mohd Affan <mohdaffanboy786@gmail.com>
2025-12-14 18:39:22 +0530

    class

---
+++ class7.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    int a[3][3],b[3][3],c[3][3],k;
+    cout<<"enter number of first matrix: ";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    cout<<"enter element of second element";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cin>>b[i][j];
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=0;
+            for(k=0;k<3;k++)
+            {
+                c[i][j]=c[i][j]+a[i][k]*b[k][j];
+            }
+             cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+     
+}
