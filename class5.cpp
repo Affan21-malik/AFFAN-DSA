47dfe0e85843289055ac7df86e813de0cadaab48
Mohd Affan <mohdaffanboy786@gmail.com>
2025-12-14 18:38:12 +0530

    class

---
+++ class5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+// sum the values
+     /*int i,sum=0;
+    int array[10];
+     
+     for(i=0;i<=9;i++) {
+ cin>>array[i];
+ sum=sum+array[i];
+     }
+cout<<sum;*/
+// small and alrgest
+     
+
+    
+    return 0;
+}
