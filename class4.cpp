d5b5862519913e3ac1b5f52316956a10be9e1973
Mohd Affan <mohdaffanboy786@gmail.com>
2025-12-14 18:37:39 +0530

    class

---
+++ class4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // array value 5 asa ha (,0,1,2,3,4............ )
+    /*int array[5]={1,16,24,4,5};
+    cout << array[3] << endl;
+    cout << array[2] << endl;*/ 
+    // value switch;
+   /* int i;
+    int array[5]={1,16,24,4,5};
+    array[4]=19;
+    for(int i=0;i<=4;i++)
+        cout<<array[i]<<endl;*/
+        //enter the value and print
+        int i;
+    int arr[10];
+     cout<<"enter 10 value"<<endl;
+     for(i=0;i<=9;i++) {
+ cin>>arr[i];
+     }
+for(i=0;i<=9;i++) {
+cout<<arr[i]<<endl;
+}
+     
+
+    
+    return 0;
+}
