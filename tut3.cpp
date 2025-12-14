92bbe53b75d9a56cee9de866b7c4da5a261f417e
Mohd Affan <mohdaffanboy786@gmail.com>
2025-12-14 18:41:59 +0530

    tut

---
+++ tut3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+/* this code created by mohd affan 
+and now his level is beginners*/
+int main() {
+    int sum=6;
+    cout<<"hello world"<<sum;
+    return 0;
+}
