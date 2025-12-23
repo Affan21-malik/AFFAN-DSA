d4e7373659967b3f9f39cc11a57524b2a6adeb6e
mohd Affan <mohdaffanboy786@gmail.com>
2025-12-23 23:28:25 +0530

    problem

---
+++ codechef problems/p12.cpp
@@ -0,0 +1,42 @@
+//643
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int num1, num2;
+        cin >> num1 >> num2;
+
+        if (2 * num2 > num1) {
+            cout << 0 << endl;
+        }
+        else {
+            
+            cout << num1 / (2 * num2) << endl;
+        }
+    }
+    return 0;
+}
+    ye mane khud ne bnaya ha
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int num1, num2;
+        cin >> num1 >> num2;
+
+        cout << num1 / (2 * num2) << endl;
+    }
+
+    return 0;
+}
