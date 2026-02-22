3970e47317c2738d4b1066710225bfa0261b42cd
mohd Affan <mohdaffanboy786@gmail.com>
2026-02-22 12:07:09 +0530

    practice

--- self practice/arr1.cpp
+++ self practice/arr1.cpp
@@ -92,6 +92,26 @@ int main() {
 }
 */
 /*
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int marks[5];
+int size =5;
+for(int i=0;i<size;i++){
+    cin>>marks[i];
+}
+int smallest = INT_MAX;
+for(int i = 0;i<size;i++){
+  if(marks[i]<smallest){
+      smallest =marks[i];
+  }
+}
+cout<<"smallest is "<<smallest;
+    return 0;
+}
+*/
+/*
 //Find smallest & largest (min / max)
 #include <iostream>
 #include <climits>
