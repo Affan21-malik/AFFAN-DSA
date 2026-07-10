9a8a24ee6a245d4f6dd919f74c1e627b29606aee
mohd Affan <mohdaffanboy786@gmail.com>
2026-07-10 11:43:36 +0530

    28

---
+++ leetcode/28.cpp
@@ -0,0 +1,65 @@
+/*
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int n = haystack.size();
+        int m = needle.size();
+
+        for (int i = 0; i <= n - m; i++) {
+
+            int j = 0;
+
+            while (j < m && haystack[i + j] == needle[j]) {
+                j++;
+            }
+
+            if (j == m)
+                return i;
+        }
+
+        return -1;
+    }
+};
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int strStr(string haystack, string needle) {
+
+    int n = haystack.size();
+    int m = needle.size();
+
+    for (int i = 0; i <= n - m; i++) {
+
+        int j = 0;
+
+        while (j < m && haystack[i + j] == needle[j]) {
+            j++;
+        }
+
+        if (j == m)
+            return i;
+    }
+
+    return -1;
+}
+
+int main() {
+
+    string haystack, needle;
+
+    cout << "Enter haystack: ";
+    cin >> haystack;
+
+    cout << "Enter needle: ";
+    cin >> needle;
+
+    int index = strStr(haystack, needle);
+
+    cout << "Index = " << index << endl;
+
+    return 0;
+}
+
