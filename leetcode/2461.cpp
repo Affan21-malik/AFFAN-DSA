5fe46712dc75b2a3b2082a26ee8bd22f5941dfa4
mohd Affan <mohdaffanboy786@gmail.com>
2026-07-26 23:00:28 +0530

    2461. Maximum Sum of Distinct Subarrays With Length K

---
+++ leetcode/2461.cpp
@@ -0,0 +1,43 @@
+/*
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        long long sum = 0;
+        long long ans = 0;
+
+        unordered_map<int, int> mp;
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+            mp[nums[i]]++;
+        }
+
+        if (mp.size() == k) {
+            ans = sum;
+        }
+
+        // Sliding window
+        for (int i = k; i < n; i++) {
+
+            sum -= nums[i - k];
+            mp[nums[i - k]]--;
+
+            if (mp[nums[i - k]] == 0) {
+                mp.erase(nums[i - k]);
+            }
+
+            sum += nums[i];
+            mp[nums[i]]++;
+
+            if (mp.size() == k) {
+                ans = max(ans, sum);
+            }
+        }
+
+        return ans;
+    }
+};
+*/
