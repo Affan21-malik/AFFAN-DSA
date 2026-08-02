629c06dd08bcc3a2cfefced75063e45a8fcd2037
mohd Affan <mohdaffanboy786@gmail.com>
2026-08-02 18:59:15 +0530

    Range Sum Query - Immutable

---
+++ leetcode/prefix/303.cpp
@@ -0,0 +1,23 @@
+/*
+class NumArray {
+public:
+    vector<int> nums;
+
+    NumArray(vector<int>& arr) {
+
+        nums = arr;
+
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+    }
+
+    int sumRange(int left, int right) {
+
+        if (left == 0)
+            return nums[right];
+
+        return nums[right] - nums[left - 1];
+    }
+};
+*/
