004afea08b387b567e9b915c722d9dfdbb3d0ab8
mohd Affan <mohdaffanboy786@gmail.com>
2026-08-02 19:01:34 +0530

    Left and Right Sum Differences

---
+++ leetcode/prefix/2574.cpp
@@ -0,0 +1,27 @@
+/*
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            totalSum += nums[i];
+        }
+
+        int leftSum = 0;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            ans.push_back(abs(leftSum - rightSum));
+
+            leftSum += nums[i];
+        }
+
+        return ans;
+    }
+};
+*/
