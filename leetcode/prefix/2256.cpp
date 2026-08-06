5c7e0f73ec8ee715b590926680f6578ba6825e3c
mohd Affan <mohdaffanboy786@gmail.com>
2026-08-06 22:19:02 +0530

    Minimum Average Difference

---
+++ leetcode/prefix/2256.cpp
@@ -0,0 +1,40 @@
+/*
+
+
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        long long totalSum=0;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            totalSum+=nums[i];
+        }
+      long long leftSum=0;
+       long long minDiff=LLONG_MAX;
+       int ans=0;
+        for(int i=0;i<nums.size();i++){
+           leftSum+=nums[i];
+          long long rightSum= totalSum-leftSum;
+          int leftAvg= leftSum/(i+1);
+          int rightAvg;
+          if(i==nums.size()-1){
+            rightAvg=0;
+          }
+          else{
+            rightAvg= rightSum/(nums.size()-i-1);
+          }
+
+          long long diff = abs(leftAvg-rightAvg);
+          if(diff<minDiff){
+            minDiff=diff;
+            ans=i;
+          }
+
+         }
+
+        return ans;
+    }
+};
+
+
+*/
