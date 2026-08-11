519c9721fa1c8cb13e0d7957bc16e201f1d98677
mohd Affan <mohdaffanboy786@gmail.com>
2026-08-11 22:34:21 +0530

    Sum of All Odd Length Subarrays

---
+++ leetcode/prefix/1588.cpp
@@ -0,0 +1,37 @@
+/*
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+
+        int n = arr.size();
+
+        vector<int> prefix(n + 1, 0);
+
+        // Prefix Sum
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + arr[i];
+        }
+
+        int ans = 0;
+
+        // All subarrays
+        for (int l = 0; l < n; l++) {
+
+            for (int r = l; r < n; r++) {
+
+                int length = r - l + 1;
+
+                // Only odd length
+                if (length % 2 == 1) {
+
+                    int sum = prefix[r + 1] - prefix[l];
+
+                    ans += sum;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+*/
