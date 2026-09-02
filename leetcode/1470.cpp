dea7b5161516060e9be092d698572d808c6f0497
mohd Affan <mohdaffanboy786@gmail.com>
2026-09-02 09:54:31 +0530

    new question

--- leetcode/1470.cpp
+++ leetcode/1470.cpp
@@ -13,4 +13,29 @@ public:
 
     }
 };
+
+
+
+
+// bina library k ok
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+
+        vector<int> ans(2 * n);
+
+        int j = 0;
+
+        for(int i = 0; i < n; i++) {
+            ans[j] = nums[i];
+            j++;
+
+            ans[j] = nums[i + n];
+            j++;
+        }
+
+        return ans;
+    }
+};
 */
