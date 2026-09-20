078b864175515819f02d47eb88e5ffbdf6516ce2
mohd Affan <mohdaffanboy786@gmail.com>
2026-09-20 11:25:51 +0530

    functions questions

---
+++ self practice/function/yt1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+ cout<<"enter number";
+cin>>n;
+if(n>10&&n<15){
+    cout<<" good marks";}
+    else if(n<10&&n>7){
+        cout<<"your are passed";
+
+    }
+    else{
+        cout<<"you fail next time try";
+    }
+
+ 
+    return 0;
+}
